78e3122f25005ee138552e04c41079ac7308a54f
Sadra <31988724+xsadra@users.noreply.github.com>
2019-05-02 18:11:26 +0200

    Add Process Callback Query method to main

--- main.php
+++ main.php
@@ -3,11 +3,12 @@
 	include_once 'core/config/config.php';
 	include_once 'core/functions/generalFunctions.php';
 	include_once 'core/functions/botFunctions.php';
-	include_once 'core/mainFunctions/processMessage.php';
-	include_once 'core/functions/messageTexts.php';
-	include_once 'core/functions/referenceUserFunctions.php';
+    include_once 'core/functions/messageTextsFa.php';
+    include_once 'core/functions/referenceUserFunctions.php';
+    include_once 'core/mainFunctions/processMessage.php';
+    include_once 'core/mainFunctions/processCallbackQuery.php';
 
-	$content = file_get_contents("php://input");
+    $content = file_get_contents("php://input");
 	$update = json_decode($content, true);
 	if($log_Update) {
 	    logTo($update,"update");
@@ -17,4 +18,7 @@
 	}
 	if(isset($update["message"])) {
 	  processMessage($update["message"]);
+	}
+	if(isset($update["callback_query"])) {
+	  processCallbackQuery($update["callback_query"]);
 	}
