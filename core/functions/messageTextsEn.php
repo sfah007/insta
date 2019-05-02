33a32c948fb17290b7e8af217a255f3afab32ff0
Sadra <31988724+xsadra@users.noreply.github.com>
2019-05-02 18:12:56 +0200

    English User Interface

--- core/functions/messageTextsEn.php
+++ core/functions/messageTextsEn.php
@@ -1,12 +1,12 @@
 <?php
 function getMsgNewUser() {
-    return "این ربات برای ذخیره عکس پروفایل، عکس های پست شده و ویدیو های پست شده در اینستاگرام است.
+    return "This Bot is for storing profile photos, photos and videos posted on Instagram.
 
-لینک مورد نظر را کپی کرده و همینجا برای ربات ارسال نمایید. ربات در پاسخ عکس و یا فیلم را برایتان ارسال میکند.";
+Copy the link and send it to the Bot. Bot will sends you a photo or video response.";
 }
 
 function getErrorMsg() {
-    return "خطا - لینک را چک نمایید ممکن است اشتباه باشد و یا پروفایل مورد نظر خصوصی باشد.";
+    return "Error - Checking the link may be incorrect or the profile is private.";
 }
 
 function getNotValidUserReferenceErrorMessage() {
@@ -37,6 +37,27 @@ function getLessPointNotificationMessage() {
     return "Your current point is less than %s, invite someone to increase the point.";
 }
 
+function getPointNotificationMessage() {
+    return "Your current point is %s";
+}
+
 function getNoEnoughPointNotificationMessage() {
     return "You don't have enough point to complete process, invite someone to increase the point.";
 }
+
+function getSuccessBlockedUserNotification() {
+    return "This User is Blocked.";
+}
+
+function getFailedBlockedUserNotification() {
+    return "You have to replay Message from User To Block.";
+}
+
+function getCurrentPointMessage() {
+    return "Current Points";
+}
+
+function getInviteLinkMessage() {
+    return "Get Invite Link";
+}
+
