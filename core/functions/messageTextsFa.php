11bfa9bd243b2862d84b20d62142dd1414a5e621
Sadra <31988724+xsadra@users.noreply.github.com>
2019-05-02 18:13:15 +0200

    Persian User Interface

---
+++ core/functions/messageTextsFa.php
@@ -0,0 +1,63 @@
+<?php
+function getMsgNewUser() {
+    return "این ربات برای ذخیره عکس پروفایل، عکس های پست شده و ویدیو های پست شده در اینستاگرام است.
+
+لینک مورد نظر را کپی کرده و همینجا برای ربات ارسال نمایید. ربات در پاسخ عکس و یا فیلم را برایتان ارسال میکند.";
+}
+
+function getErrorMsg() {
+    return "خطا - لینک را چک نمایید ممکن است اشتباه باشد و یا پروفایل مورد نظر خصوصی باشد.";
+}
+
+function getNotValidUserReferenceErrorMessage() {
+    return "شناسه مرجع لینک نامعتبر است. لینک را برسی کنید و یا این خطا را نادیده بگیرید.";
+}
+
+function getAddPointNotificationToUserReferenceMessage() {
+    return "شما مقداری امتیاز به دست آوردید، شخصی از لینک معرفی شما استفاده کرده است.";
+}
+
+function getUserAlreadyExistMessage() {
+    return "شما قبلا در این ربات ثبت نام کرده اید.";
+}
+
+function getReferenceIdAlreadyExistMessage() {
+    return "شما یک بار از این لینک استفاده کرده اید. امکان استفاده مجدد از آن مقدور نیست.";
+}
+
+function getRegisterSuccessMessage() {
+    return "ثبت نام شما در ربات با موفقیت انجام شد.";
+}
+
+function getInviteMessage() {
+    return "برای دانلود عکسها و ویدیوها از اینستاگرام به ما بپیوندید.";
+}
+
+function getLessPointNotificationMessage() {
+    return "امتیاز فعلی شما کمتر از %s است، برای بدست اوردن امتیاز چند نفر را از طریق لینک به ربات دعوت کنید.";
+}
+
+function getPointNotificationMessage() {
+    return "امتیاز فعلی شما %s است،";
+}
+
+function getNoEnoughPointNotificationMessage() {
+    return "موجودی امتیاز شما صفر است، برای استفاده از امکانات ربات دیگران را از طریق لینک عضوگیری به ربات دعوت کنید.";
+}
+
+function getSuccessBlockedUserNotification() {
+    return "کاربر مسدود شد.";
+}
+
+function getFailedBlockedUserNotification() {
+        return "شما باید یک پیام از کاربر را  برای مسدود کردن او ریپلای کنید.";
+}
+
+function getCurrentPointMessage() {
+    return "امتیاز من";
+}
+
+function getInviteLinkMessage() {
+    return "لینک دعوت";
+}
+
